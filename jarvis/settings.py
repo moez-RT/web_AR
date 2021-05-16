351ffa33ff298e4fb114a536d774d211b559ae60
Moez Saidi <saidi.moez.2014@gmail.com>
2021-05-16 22:27:19 +0200

    updated

--- jarvis/settings.py
+++ jarvis/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'phonenumber_field',
 ]
 
@@ -86,15 +86,21 @@ WSGI_APPLICATION = 'jarvis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'jarvis',
+#         'USER': 'root',
+#         'PASSWORD': 'jarvis',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'jarvis',
-        'USER': 'root',
-        'PASSWORD': 'jarvis',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 
